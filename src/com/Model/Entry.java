ecee54d8a442c2c3f6ca77867a27aae69074c7d2
pisharody <dpis3003@7-11.com>
2026-03-14 19:51:08 +0530

    UC-11 has been completed.

--- src/com/Model/Entry.java
+++ src/com/Model/Entry.java
@@ -1,22 +1,28 @@
 package com.Model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class Entry {
     private String name, phone, email;
-
+    private List<String> tags = new ArrayList<>();
     protected Entry(EntryBuilder b) {
         this.name = b.getName();
         this.phone = b.getPhone();
         this.email = b.getEmail();
     }
-
     public String getName() { return name; }
     public String getPhone() { return phone; }
     public String getEmail() { return email; }
-
+   public void addTag(String t) { this.tags.add(t.toUpperCase()); }
+    public List<String> getTags() { return tags; }
     public abstract String getEntryType();
-
     @Override
     public String toString() {
-        return "[" + getEntryType() + "] " + name + " | " + phone + " | " + email;
+        String base = "[" + getEntryType() + "] " + name + " | " + phone + " | " + email;
+        if (!tags.isEmpty()) {
+            base += " | Tags: " + tags.toString();
+        }
+        return base;
     }
 }
